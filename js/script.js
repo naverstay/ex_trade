85059a2aa598ed79ec5de5d166378080c4d09298
naverstay <3a4f044785G->
2016-06-16 09:28:09 +0300

    wallet

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
+var $body;
+
 $(function ($) {
-    console.log(100);
+
+    $body = $('body');
+
+    $body.delegate('.selectBox', 'click', function () {
+
+        var firedEl = $(this);
+
+        if (firedEl.closest('.selectBox').hasClass('disabled')) return false;
+
+        firedEl.toggleClass('opened');
+
+        return false;
+
+    }).delegate('.selectItem', 'click', function () {
+
+        var firedEl = $(this);
+
+        firedEl.closest('.selectBox').removeClass('opened').find('.selectVal').html(firedEl.html());
+
+        return false;
+
+    });
 
 
 });
